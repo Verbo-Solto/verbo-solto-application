2df82ce77c9bcaa5f107f9f26eb74862edab45db
paulo9631 <paulov14@alu.ufc.br>
2025-07-02 01:58:16 -0300

    Feat: Criar-obras

--- back/obras_vs/views.py
+++ back/obras_vs/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets, permissions, status, generics, filters
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
-from .models import Obra, Tag
-from .serializers import ObraSerializer, TagSerializer
+from .models import Obra, Tag, Colecao, Capitulo
+from .serializers import ObraSerializer, TagSerializer, CapituloSerializer, ColecaoSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated, SAFE_METHODS, BasePermission
 
@@ -51,7 +51,41 @@ class ObraViewSet(viewsets.ModelViewSet):
             "avaliacao_media": obra.avaliacao_media,
         })
 
+    @action(detail=False, methods=['get'], url_path='minhas-obras', permission_classes=[IsAuthenticated])
+    def minhas_obras(self, request):
+        obras = Obra.objects.filter(autor=request.user)
+        serializer = self.get_serializer(obras, many=True)
+        return Response(serializer.data)
+
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [permissions.AllowAny]
+
+from rest_framework import mixins
+
+class ColecaoViewSet(viewsets.ModelViewSet):
+    queryset = Colecao.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = ColecaoSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if not user or not user.is_authenticated:
+            self.permission_denied(self.request, message="Autenticação obrigatória para acessar coleções.")
+        return Colecao.objects.filter(autor=user)
+
+    def perform_create(self, serializer):
+        serializer.save(autor=self.request.user)
+
+class CapituloViewSet(viewsets.ModelViewSet):
+    queryset = Capitulo.objects.all()
+    serializer_class = CapituloSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Só retorna capítulos das coleções do usuário autenticado
+        return Capitulo.objects.filter(colecao__autor=self.request.user)
+        # Só retorna capítulos das coleções do usuário autenticado
+        return Capitulo.objects.filter(colecao__autor=self.request.user)
+        return Capitulo.objects.filter(colecao__autor=self.request.user)
